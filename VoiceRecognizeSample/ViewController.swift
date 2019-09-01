d92259f1837b9988886595a190ca3d9fcfd8c8f1
TomokiIshimine <ishitoki47259@gmail.com>
2019-09-01 21:54:56 +0900

    Initial Commit

---
+++ VoiceRecognizeSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VoiceRecognizeSample
+//
+//  Created by 伊志嶺朝輝 on 2019/09/01.
+//  Copyright © 2019 TomRock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
